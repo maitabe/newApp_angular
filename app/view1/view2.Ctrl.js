37b4be73e63a52ade3d23ab89a734dc92aca6c95
maitabe <martushb@gmail.com>
2015-03-17 19:54:07 +0200

    second tab html

---
+++ app/view1/view2.Ctrl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Martha on 3/17/2015.
+ */
+(function () {
+    'use strict';
+    var app = angular.module('firstApp');
+
+    app.controller('view2Ctrl', function ($scope, $http){
+
+
+
+    });
+
+}());
+
+
+
+
